6b89a8d56385002ea950de022d1b8482f9c88f3b
gardners <paul@servalproject.org>
2014-05-07 16:46:14 +0930

    add explanatory notes on using JNI interface.

--- src/org/servalproject/succinctdata/jni.java
+++ src/org/servalproject/succinctdata/jni.java
@@ -1,5 +1,17 @@
 package org.servalproject.succinctdata;
 
+/*
+ * To use these in other classes, you need:
+ * 
+ *      import org.servalproject.succinctdata.jni;
+ * 
+ * and then to call, use something like:
+ * 
+ * 		// XXX Testing JNI
+ *      org.servalproject.succinctdata.jni.xml2succinctdata("foo","bar","quux");
+ *
+ */
+
 public class jni {
 
 	public static native byte[] xml2succinctdata(String xmlforminstance, String recipename, String succinctpath);	
