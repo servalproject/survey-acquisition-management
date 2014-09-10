e98a04f53e37d3c810e5f844fb396b7ec6c09ba4
gardners <paul@servalproject.org>
2014-09-10 11:21:51 +0930

    break update CSV out separate from UI in preparation for doing CSV update etc automatically on SMS reception.

--- src/org/magdaaproject/sam/SurveyFormsActivity.java
+++ src/org/magdaaproject/sam/SurveyFormsActivity.java
@@ -151,148 +151,38 @@ public class SurveyFormsActivity extends FragmentActivity implements OnClickList
 		mCSVButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View arg0) {
-				String succinctPath = Environment.getExternalStorageDirectory().getPath()+
-				getString(R.string.system_file_path_succinct_specification_files_path);
-				String rxSpoolDir = Environment.getExternalStorageDirectory().getPath()+
-						getString(R.string.system_file_path_succinct_data_rxspool_dir);
 				String outputDir = Environment.getExternalStorageDirectory().getPath()+
-						getString(R.string.system_file_path_succinct_data_output_dir);
+						getString(R.string.system_file_path_succinct_data_output_dir);					
 				
-				//Create Rxspool directory
-				File dir = new File(Environment.getExternalStorageDirectory(),
-						getString(R.string.system_file_path_succinct_data_rxspool_dir));
-				dir.mkdirs();
-				
-				//Create ouput directory
-				File dirOutput = new File(Environment.getExternalStorageDirectory(),
-						getString(R.string.system_file_path_succinct_data_output_dir));
-				dirOutput.mkdirs();
-				
-				
-				// Read any new SMS messages and put in the rxspool directory for processing
-				Cursor cursor = getContentResolver().query(Uri.parse("content://sms/inbox"), null, null, null, null);
-				cursor.moveToFirst();
-
-				do{
-				   try {
-					   	//Get content from SMS
-					   	String msgData = "";
-					   	int idx = cursor.getColumnIndex("body");				   
-					   	msgData = cursor.getString(idx);
-					   	
-					   	//Check if no space in the SMS, avoid reading not succinct-data SMS.
-					   	if (msgData.indexOf(' ')==-1) {
-						   	byte[] decodedBytes = android.util.Base64.decode(msgData,android.util.Base64.DEFAULT);
-						   
-						   	//Get MD5 Hash from content
-						   	byte[] b = MessageDigest.getInstance("MD5").digest(decodedBytes);
-							
-						   	//Try to see if file already exists in Rxspool
-						   	String filename = String.format("%02x%02x%02x%02x%02x%02x.sd", b[0],b[1],b[2],b[3],b[4],b[5]);
-							File file = new File(dir, filename);
-							if (!file.exists()) {
-								// Write succinct data to file
-								FileOutputStream f = new FileOutputStream(file);
-								f.write(decodedBytes);
-								f.close();
-							} else {
-								Log.d("SAM",filename+" already exists in Rxspool, dont need to read older SMS.");	
-								break;
+				// More usefully for demo, show some CSV output from the first CSV file in the
+				// output directory.
+				TextView csvTextView = (TextView) findViewById(R.id.survey_forms_ui_csv_data);
+						
+				File[] csvFiles = new File(outputDir,"csv").listFiles();
+				String csvData = "";
+				for (File csvFile : csvFiles) {
+					String fileContents = "";
+							try {
+								fileContents = new Scanner(csvFile).useDelimiter("\\Z").next();
+							} catch (Exception e) {
 							}
-					   	} else {
-							Log.d("SAM","This SMS contains spaces,it's not a succinct-data.");	
+							csvData = csvData + fileContents;					
 						}
-					} catch (Exception e) {
-						// TODO Auto-generated catch block
-						// e.printStackTrace();					
-					}
-				}while(cursor.moveToNext());
-				
-				
-				Log.d("SAM","About to call smac");
-				// org.servalproject.succinctdata.jni.updatecsv(succinctPath,rxSpoolDir,outputDir);
-				
-				
-				try {
-					  AssetManager assetManager = getAssets();
-					  InputStream in = assetManager.open("smac");
-
-			          File outDir = new File(getFilesDir().getPath()+ "/bin");
-			          outDir.mkdirs();
-			          File outFile = new File(getFilesDir().getPath()+ "/bin", "smac");
-			          FileOutputStream out = new FileOutputStream(outFile);
-			          int len;
-			          byte[] buff = new byte[8192];
-			  		  while ((len = in.read(buff)) > 0) {
-						out.write(buff, 0, len);
-					  }
-			          in.close();
-			          in = null;
-			          out.flush();
-			          out.close();
-			          out = null;
-			          outFile.setExecutable(true);			          
-			        } catch(IOException e) {
-						Log.e("tag", "Failed to load assets. Problem may be because ndk-build has not been done before building project.", e);
-			            Log.e("tag", "Failed to copy asset file smac", e);
-			        }       
-				
-				String cmd = getFilesDir().getPath()+ "/bin/smac"; 
-				Process proc;
-				try {
-					proc = new ProcessBuilder(cmd,"recipe", "decompress", succinctPath,rxSpoolDir,outputDir).redirectErrorStream(true).start();
-					DataInputStream in = new DataInputStream(proc.getInputStream());
-					OutputStream out = proc.getOutputStream();
-					proc.waitFor();
-				} catch (Throwable e) {
-					Log.e("tag", "Failed to run smac", e);
-				}
-				
-				// Create map visualisations
-				try {
-					proc = new ProcessBuilder(cmd,"recipe", "map", succinctPath,outputDir).redirectErrorStream(true).start();
-					DataInputStream in = new DataInputStream(proc.getInputStream());
-					OutputStream out = proc.getOutputStream();
-					proc.waitFor();
-				} catch (Throwable e) {
-					Log.e("tag", "Failed to run smac", e);
-				}
-				
-				//Send map visualisations online
-				upLoadServerUri = "http://dev.malossane.fr/modules/serval/UploadToServer.php";
-				String [] allFilesToUpload = new File(outputDir+"/maps/").list();
-				for (int i=0; i<allFilesToUpload.length;i++){ 
-					uploadFile(outputDir+"/maps/"+allFilesToUpload[i]);
-				}
-				
+				csvTextView.setText(csvData);
+				csvTextView.setMovementMethod(new ScrollingMovementMethod());					
+								
 				// Now open chooser to pick a file manager to view the directory
-				// This doesn't actually work with most file managers.
+				// This doesn't actually work with most file managers, but hopefully
+				// will launch a browser
 				Intent intent = new Intent();
 				File file = new File(outputDir+"/maps/index.html");
 				Uri uri = Uri.fromFile(file);				
 				intent.setAction(Intent.ACTION_VIEW);
 				intent.setDataAndType(uri,"text/html");
 				startActivity(intent);
-
-				// More usefully for demo, show some CSV output from the first CSV file in the
-				// output directory.
-				TextView csvTextView = (TextView) findViewById(R.id.survey_forms_ui_csv_data);
-				
-				File[] csvFiles = new File(outputDir,"csv").listFiles();
-				String csvData = "";
-				for (File csvFile : csvFiles) {
-					String fileContents = "";
-					try {
-						fileContents = new Scanner(csvFile).useDelimiter("\\Z").next();
-					} catch (Exception e) {
-					}
-					csvData = csvData + fileContents;					
-				}
-				csvTextView.setText(csvData);
-				csvTextView.setMovementMethod(new ScrollingMovementMethod());
 			}
-			
 		});
+		
 
 		Button mXMLButton = (Button) findViewById(R.id.getxml);
 		mXMLButton.setOnClickListener(new View.OnClickListener() {
@@ -582,108 +472,5 @@ public class SurveyFormsActivity extends FragmentActivity implements OnClickList
 		}
 	}
 	
-	
-	public int uploadFile(String sourceFileUri) {
-		//WARNING, TO DELETE
-		//THE URL IS DONE IN MAIN THREAD RIGHT NOW, TO FIX
-		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-		StrictMode.setThreadPolicy(policy); 
-        
-        String fileName = sourceFileUri;
-
-        HttpURLConnection conn = null;
-        DataOutputStream dos = null; 
-        String lineEnd = "\r\n";
-        String twoHyphens = "--";
-        String boundary = "*****";
-        int bytesRead, bytesAvailable, bufferSize;
-        byte[] buffer;
-        int maxBufferSize = 1 * 1024 * 1024;
-        File sourceFile = new File(sourceFileUri);
-         
-        if (!sourceFile.isFile()) {
-             Log.e("uploadFile", "Source File not exist :"+fileName);
-             return 0; 
-        }
-        else
-        {
-             try {
-                  
-                   // open a URL connection to the Servlet
-                 FileInputStream fileInputStream = new FileInputStream(sourceFile);
-                 URL url = new URL(upLoadServerUri);
-                  
-                 // Open a HTTP  connection to  the URL
-                 conn = (HttpURLConnection) url.openConnection();
-                 conn.setDoInput(true); // Allow Inputs
-                 conn.setDoOutput(true); // Allow Outputs
-                 conn.setUseCaches(false); // Don't use a Cached Copy
-                 conn.setRequestMethod("POST");
-                 conn.setRequestProperty("Connection", "Keep-Alive");
-                 conn.setRequestProperty("ENCTYPE", "multipart/form-data");
-                 conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
-                 conn.setRequestProperty("uploaded_file", fileName);
-                  
-                 dos = new DataOutputStream(conn.getOutputStream());
-        
-                 dos.writeBytes(twoHyphens + boundary + lineEnd);
-                 dos.writeBytes("Content-Disposition: form-data; name=\"uploaded_file\";filename=\""
-                                           + fileName + "\"" + lineEnd);
-                  
-                 dos.writeBytes(lineEnd);
-        
-                 // create a buffer of  maximum size
-                 bytesAvailable = fileInputStream.available();
-        
-                 bufferSize = Math.min(bytesAvailable, maxBufferSize);
-                 buffer = new byte[bufferSize];
-        
-                 // read file and write it into form...
-                 bytesRead = fileInputStream.read(buffer, 0, bufferSize); 
-                    
-                 while (bytesRead > 0) {
-                      
-                   dos.write(buffer, 0, bufferSize);
-                   bytesAvailable = fileInputStream.available();
-                   bufferSize = Math.min(bytesAvailable, maxBufferSize);
-                   bytesRead = fileInputStream.read(buffer, 0, bufferSize);  
-                    
-                  }
-        
-                 // send multipart form data necesssary after file data...
-                 dos.writeBytes(lineEnd);
-                 dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
-        
-                 // Responses from the server (code and message)
-                 serverResponseCode = conn.getResponseCode();
-                 String serverResponseMessage = conn.getResponseMessage();
-                   
-                 Log.i("uploadFile", "HTTP Response is : "
-                         + serverResponseMessage + ": " + serverResponseCode);
-                  
-                 if(serverResponseCode == 200){
-                	 String msg = "File Upload Completed.\n\n See uploaded file here : \n\n"
-                             +" http://dev.malossane.fr/modules/serval/uploads/"
-                             +fileName;
-                	 Log.i("uploadFile", msg);
-                	             
-                 }   
-                  
-                 //close the streams //
-                 fileInputStream.close();
-                 dos.flush();
-                 dos.close();
-                   
-            } catch (MalformedURLException ex) {
-                ex.printStackTrace();  
-                Log.e("Upload file to server", "error: " + ex.getMessage(), ex); 
-            } catch (Exception e) {
-                e.printStackTrace();
-                Log.e("Upload file to server Exception", "Exception : "
-                                                 + e.getMessage(), e); 
-            }   
-            return serverResponseCode;
-             
-         } // End else block
-       } 
 }
+	
